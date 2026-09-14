ca63c88868201038de7e5952f85ad37c187709c9
pdlabs <accounts@performancedigital.com>
2026-09-14 16:27:11 -0500

    Update app.js

--- app.js
+++ app.js
@@ -1,1 +1,20 @@
+document.addEventListener("DOMContentLoaded", () => {
 
+  const printButton =
+    document.getElementById("print-report");
+
+
+  if (printButton) {
+
+    printButton.addEventListener(
+      "click",
+      () => {
+
+        window.print();
+
+      }
+    );
+
+  }
+
+});
