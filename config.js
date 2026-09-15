b52737cbdfaec88c5853afbb76619fb533f09178
pdlabs <accounts@performancedigital.com>
2026-09-15 09:22:27 -0500

    added webhook url config.js

--- config.js
+++ config.js
@@ -1,1 +1,55 @@
+window.REPORT_CONFIG = {
 
+  apiUrl: "https://performancedigital.app.n8n.cloud/webhook/gfs-report-v1",
+
+  advertiserId: "35759",
+
+  clientName: "Generators for Sale",
+
+  markets: {
+    all: null,
+    omaha: "53305",
+    kansasCity: "53313"
+  },
+
+  campaigns: {
+
+    "120797": {
+      market: "Omaha",
+      journeyId: "53305",
+      channel: "CTV"
+    },
+
+    "120801": {
+      market: "Omaha",
+      journeyId: "53305",
+      channel: "Native"
+    },
+
+    "120809": {
+      market: "Omaha",
+      journeyId: "53305",
+      channel: "Display"
+    },
+
+    "120811": {
+      market: "Kansas City",
+      journeyId: "53313",
+      channel: "CTV"
+    },
+
+    "120813": {
+      market: "Kansas City",
+      journeyId: "53313",
+      channel: "Native"
+    },
+
+    "120815": {
+      market: "Kansas City",
+      journeyId: "53313",
+      channel: "Display"
+    }
+
+  }
+
+};
